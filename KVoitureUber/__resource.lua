81b9f0661111b34dfaf5b8fae0acc5d22838aeb6
Kaaiitoo <95180329+Kaaiitoo@users.noreply.github.com>
2023-12-31 16:19:44 +0100

    Add files via upload

---
+++ KVoitureUber/__resource.lua
@@ -0,0 +1,107 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+---- merco UBER
+
+files {
+
+
+
+    '12cls63/handling.meta',
+
+	'12cls63/vehiclelayouts.meta',
+
+    '12cls63/vehicles.meta',
+
+    '12cls63/carvariations.meta',
+
+    '12cls63/carcols.meta',
+
+
+
+    
+
+}
+
+
+
+data_file 'HANDLING_FILE' '12cls63/handling.meta'
+
+data_file 'VEHICLE_LAYOUTS_FILE' '12cls63/vehiclelayouts.meta'
+
+data_file 'VEHICLE_METADATA_FILE' '12cls63/vehicles.meta'
+
+data_file 'CARCOLS_FILE' '12cls63/carcols.meta'
+
+data_file 'VEHICLE_VARIATION_FILE' '12cls63/carvariations.meta'
+
+
+-- BMW I8 UBER 
+
+files {
+
+
+
+    'i8/handling.meta',
+
+	'i8/vehiclelayouts.meta',
+
+    'i8/vehicles.meta',
+
+    'i8/carvariations.meta',
+
+    'i8/carcols.meta',
+
+
+
+    
+
+}
+
+
+
+data_file 'HANDLING_FILE' 'i8/handling.meta'
+
+data_file 'VEHICLE_LAYOUTS_FILE' 'i8/vehiclelayouts.meta'
+
+data_file 'VEHICLE_METADATA_FILE' 'i8/vehicles.meta'
+
+data_file 'CARCOLS_FILE' 'i8/carcols.meta'
+
+data_file 'VEHICLE_VARIATION_FILE' 'i8/carvariations.meta'
+
+-- RANGE ROVER UBER
+
+files {
+
+
+
+    'HamannEvoque/handling.meta',
+
+	'HamannEvoque/vehiclelayouts.meta',
+
+    'HamannEvoque/vehicles.meta',
+
+    'HamannEvoque/carvariations.meta',
+
+    'HamannEvoque/carcols.meta',
+
+
+
+    
+
+}
+
+
+
+data_file 'HANDLING_FILE' 'HamannEvoque/handling.meta'
+
+data_file 'VEHICLE_LAYOUTS_FILE' 'HamannEvoque/vehiclelayouts.meta'
+
+data_file 'VEHICLE_METADATA_FILE' 'HamannEvoque/vehicles.meta'
+
+data_file 'CARCOLS_FILE' 'HamannEvoque/carcols.meta'
+
+data_file 'VEHICLE_VARIATION_FILE' 'HamannEvoque/carvariations.meta'
+
+
+--######################## CREE PAR KAITO#0511 #####################
